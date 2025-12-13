f54ec743da3a2f5285ed77e12d7c47e7225b4e7d
Sagnik Dey <your_email@example.com>
2025-12-13 21:53:51 +0530

    video load issue

--- app/components/illustration.tsx
+++ app/components/illustration.tsx
@@ -1,17 +1,18 @@
 export default function Illustration() {
   return (
     <div className="w-[90vw] max-w-6xl mx-auto mt-[-30px] border border-transparent rounded-2xl shadow-md overflow-hidden ">
-      <video
-        className="w-full h-auto block" // h-auto lets height adjust naturally
-        src="218955_small.mp4" // Ensure this file is in your public folder
-        autoPlay
-        loop
-        muted
-        playsInline
-       // Remove this if you don't want play/pause buttons
-      >
-        Your browser does not support the video tag.
-      </video>
+     <video
+  className="w-full h-auto block"
+  src="/218955_small.mp4" // Ensure this file is in your public folder
+  autoPlay
+  loop
+  muted
+  playsInline
+  preload="auto" // Preload the video for faster start
+>
+  Your browser does not support the video tag.
+</video>
+
     </div>
   );
 }
